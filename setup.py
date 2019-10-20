7a632c134767feb2c4c15f7cdac0c2396fa7b235
Gabriele N. Tornetta <phoenix1987@gmail.com>
2019-10-20 17:12:01 +0100

    Update sources for the v1 release

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 from os import path
 
-# Always prefer setuptools over distutils
 from setuptools import find_packages, setup
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
     name="python-austin",
-    version="0.2.0",
+    version="0.3.0",
     description=("Python wrapper for Austin, the frame stack sampler for CPython"),
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -24,13 +22,14 @@ setup(
         "License :: OSI Approved :: GPLv3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     keywords="profiler stack sampler",
     packages=find_packages(exclude=["docs", "test"]),
     include_package_data=True,
     python_requires=">=3.6",
     install_requires=["aiohttp", "psutil", "pyfiglet"],
-    extras_require={"dev": ["check-manifest"], "test": ["coverage"]},  # TODO
+    # extras_require={"dev": ["check-manifest"], "test": ["coverage"]},  # TODO
     entry_points={
         "console_scripts": [
             "austin-tui=austin.main:main",
