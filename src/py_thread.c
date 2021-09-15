P403n1x87/austin

commit 7184414132ffffe1a20905b401a08b543dd9cb3f

feat: austinp variant (#85)
Sample native and kernel stacks on Linux using `ptrace`

src/py_thread.c
@@ -23,6 +23,8 @@
 #define PY_THREAD_C
 
 #include <string.h>
+#include <sys/types.h>
+#include <signal.h>
 
 #include "argparse.h"
 #include "error.h"
@@ -77,7 +79,13 @@ typedef struct frame {
 } py_frame_t;
 
 
-py_frame_t * _stack = NULL;
+py_frame_t * _stack  = NULL;
+size_t       _stackp = 0;
+
+#ifdef NATIVE
+void          ** _tids      = NULL;
+unsigned char *  _tids_idle = NULL;
+#endif
 
 
 // ---- PyCode ----------------------------------------------------------------
@@ -289,9 +297,10 @@ _py_code__fill_from_raddr(py_code_t * self, raddr_t * raddr, int lasti) {
 
 // ----------------------------------------------------------------------------
 static inline int
-_py_frame__fill_from_raddr(py_frame_t * self, raddr_t * raddr) {
+_py_frame_fill_from_raddr(raddr_t * raddr) {
   PyFrameObject frame;
 
+  py_frame_t * self = _stack + _stackp;
   self->invalid = 1;
 
   if (fail(copy_from_raddr_v(raddr, frame, py_v->py_frame.size))) {
@@ -318,13 +327,19 @@ _py_frame__fill_from_raddr(py_frame_t * self, raddr_t * raddr) {
 
   self->invalid = 0;
 
+  _stackp++;
+
   SUCCESS;
 }
 
 
 // ----------------------------------------------------------------------------
 static inline int
-_py_frame__prev(py_frame_t * self) {
+_py_frame__prev() {
+  if (_stackp <= 0)
+    FAIL;
+
+  py_frame_t * self = _stack + _stackp - 1;
   if (!isvalid(self) || !isvalid(self->prev_raddr.addr))
     FAIL;
 
@@ -333,25 +348,112 @@ _py_frame__prev(py_frame_t * self) {
     .addr = self->prev_raddr.addr
   };
 
-  return _py_frame__fill_from_raddr(self + 1, &prev_raddr);
+  return _py_frame_fill_from_raddr(&prev_raddr);
 }
 
 
 // ----------------------------------------------------------------------------
 static inline void
 _py_thread__unwind_frame_stack(py_thread_t * self) {
-  register size_t i = 0;
-  while (success(_py_frame__prev(_stack + i)) && i < MAX_STACK_SIZE) {
-    if (_stack[++i].invalid) {
-      log_d("Frame number %d is invalid", i);
+  size_t basep = _stackp;
+
+  while (success(_py_frame__prev()) && _stackp < MAX_STACK_SIZE) {
+    if (_stack[_stackp-1].invalid) {
+      log_d("Frame number %d is invalid", _stackp - basep);
       return;
     }
   }
-  if (i >= MAX_STACK_SIZE)
+  if (_stackp >= MAX_STACK_SIZE)
     log_w("Frames limit reached. Discarding the rest");
-  self->stack_height += i;
+  
+  self->stack_height += _stackp - basep;
 }
 
+
+#ifdef NATIVE
+// ----------------------------------------------------------------------------
+void
+py_thread__set_idle(py_thread_t * self) {
+  size_t index  = self->tid >> 3;
+  int    offset = self->tid & 7;
+
+  unsigned char idle_bit = _py_thread__is_idle(self) << offset;
+  if (idle_bit) {
+    _tids_idle[index] |= idle_bit;
+  } else {
+    _tids_idle[index] &= ~idle_bit;
+  }
+}
+
+// ----------------------------------------------------------------------------
+static inline int
+_py_thread__unwind_kernel_frame_stack(py_thread_t * self) {
+  FILE   * stack_file = NULL;
+  char   * line       = NULL;
+  size_t   len        = 0;
+  char     stack_path[48];
+
+  sprintf(stack_path, "/proc/%d/task/%ld/stack", self->proc->pid, self->tid);
+  stack_file = fopen(stack_path, "r");
+  if (!isvalid(stack_file))
+    FAIL;
+
+  log_t("linux: unwinding kernel stack");
+
+  while (getline(&line, &len, stack_file) != -1) {
+    char * b = strchr(line, ']');
+    if (!isvalid(b))
+      continue;
+    char * e = strchr(++b, '+');
+    if (isvalid(e))
+      *e = 0;
+    strcpy(_stack[_stackp].code.scope, ++b);
+    strcpy(_stack[_stackp].code.filename, "kernel");
+    sfree(line);
+    _stackp++;  // TODO: Decide whether to decremet this by 2 before returning.
+  }
+
+  fclose(stack_file);
+
+  SUCCESS;
+}
+
+
+// ----------------------------------------------------------------------------
+static inline int
+_py_thread__unwind_native_frame_stack(py_thread_t * self) {
+  void *context = _tids[self->tid];
+  unw_cursor_t cursor;
+
+  while (unw_init_remote(&cursor, self->proc->unwind.as, context));
+
+  do {
+    unw_word_t offset, pc;
+    
+    if (unw_get_reg(&cursor, UNW_REG_IP, &pc)) {
+      log_e("libunwind: cannot read program counter\n");
+      FAIL;
+    }
+
+    if (unw_get_proc_name(&cursor, _stack[_stackp].code.scope, MAXLEN, &offset) == 0) {
+      // To retrieve source name and line number we would need to
+      // - resolve the PC to a map to get the binary path
+      // - use the offset with the binary to get the line number from DWARF (see
+      //   https://kernel.googlesource.com/pub/scm/linux/kernel/git/hjl/binutils/+/hjl/secondary/binutils/addr2line.c)
+      _stack[_stackp].code.lineno = offset;
+    }
+    else {
+      strcpy(_stack[_stackp].code.scope, "<unnamed>");
+      _stack[_stackp].code.lineno = 0;
+    }
+    sprintf(_stack[_stackp].code.filename, "native@%lx", pc);
+
+    _stackp++;
+  } while (_stackp < MAX_STACK_SIZE && unw_step(&cursor) > 0);
+  
+  SUCCESS;
+}
+#endif
 
 // ---- PUBLIC ----------------------------------------------------------------
 
@@ -370,7 +472,7 @@ py_thread__fill_from_raddr(py_thread_t * self, raddr_t * raddr, py_proc_t * proc
 
   if (V_FIELD(void*, ts, py_thread, o_frame) != NULL) {
     raddr_t frame_raddr = { .pid = raddr->pid, .addr = V_FIELD(void*, ts, py_thread, o_frame) };
-    if (fail(_py_frame__fill_from_raddr(_stack, &frame_raddr))) {
+    if (fail(_py_frame_fill_from_raddr(&frame_raddr))) {
       log_d("Failed to fill last frame");
       SUCCESS;
     }
@@ -412,18 +514,37 @@ py_thread__fill_from_raddr(py_thread_t * self, raddr_t * raddr, py_proc_t * proc
         _pthread_buffer
     ))) {
       self->tid = (uintptr_t) _pthread_buffer[_pthread_tid_offset];
+      #ifdef NATIVE
+      // TODO: If a TID is reused we will never seize it!
+      if (!isvalid(_tids[self->tid])) {
+        if (fail(ptrace(PTRACE_SEIZE, self->tid, 0, 0))) {
+          log_e("ptrace: cannot seize thread %d: %d\n", self->tid, errno);
+          FAIL;
+        }
+        else {
+          log_d("ptrace: thread %d seized", self->tid);
+        }
+        _tids[self->tid] = _UPT_create(self->tid);
+        if (!isvalid(_tids[self->tid])) {
+          log_e("libunwind: failed to create context for thread %d", self->tid);
+          FAIL;
+        }
+      }
+      #endif
     }
   }
   #endif
 
   self->invalid = 0;
   SUCCESS;
-}
+} /* py_thread__fill_from_raddr */
 
 
 // ----------------------------------------------------------------------------
 int
 py_thread__next(py_thread_t * self) {
+  _stackp = 0;
+  
   if (!isvalid(self->next_raddr.addr))
     FAIL;
 
@@ -469,24 +590,82 @@ py_thread__print_collapsed_stack(py_thread_t * self, ctime_t time_delta, ssize_t
 
   int is_idle = FALSE;
   if (pargs.full || pargs.sleepless) {
+    #ifdef NATIVE
+    size_t index  = self->tid >> 3;
+    int    offset = self->tid & 7;
+
+    is_idle = _tids_idle[index] & (1 << offset);
+    #else
     is_idle = _py_thread__is_idle(self);
-    if (!pargs.full && is_idle && pargs.sleepless)
+    #endif
+    if (!pargs.full && is_idle && pargs.sleepless) {
+      #ifdef NATIVE
+      // If we don't sample the threads stall :(
+      _py_thread__unwind_native_frame_stack(self);
+      #endif
       return;
+    }
   }
 
   // Group entries by thread.
   fprintf(pargs.output_file, SAMPLE_HEAD, self->proc->pid, self->tid);
 
+
+  #ifdef NATIVE
+  _stackp = 0;
+
+  // We sample the kernel frame stack BEFORE interrupting because otherwise
+  // we would see the ptrace syscall call stack, which is not very interesting.
+  // The downside is that the kernel stack might not be in sync with the other
+  // ones.
+  if (pargs.kernel) {
+    log_t("unwinding kernel stack");
+    _py_thread__unwind_kernel_frame_stack(self);
+  }
+  _py_thread__unwind_native_frame_stack(self);
+
+  size_t basep = _stackp;
+  // Update the thread state to improve guarantees that it will be in sync with
+  // the native stack just collected
+  py_thread__fill_from_raddr(self, &self->raddr, self->proc);
+  #endif
+
   if (self->stack_height) {
     _py_thread__unwind_frame_stack(self);
 
+    #ifndef NATIVE
     // Append frames
     register int i = self->stack_height;
     while (i > 0) {
       py_code_t code = _stack[--i].code;
       fprintf(pargs.output_file, pargs.format, code.filename, code.scope, code.lineno);
     }
+    #endif
   }
+
+  #ifdef NATIVE
+
+  register int i = _stackp;
+  register int j = basep;
+
+  py_code_t * code;
+  while (j-- > 0) {
+    if (strstr(_stack[j].code.scope, "PyEval_EvalFrame")) {
+      code = ((i <= basep) ? &(_stack[j].code) : &(_stack[--i].code));
+    }
+    else {
+      code = &(_stack[j].code);
+    }
+    fprintf(pargs.output_file, pargs.format, code->filename, code->scope, code->lineno);
+  }
+  if (i != basep) {
+    log_e("Stack mismatch: left with %d Python frames after interleaving", i - basep);
+    austin_errno = ETHREADINV;
+    #ifdef DEBUG
+    fprintf(pargs.output_file, ";:%ld FRAMES LEFT:", i - basep);
+    #endif
+  }
+  #endif
 
   if (pargs.gc && py_proc__is_gc_collecting(self->proc) == TRUE) {
     fprintf(pargs.output_file, ";:GC:");
@@ -516,7 +695,7 @@ py_thread__print_collapsed_stack(py_thread_t * self, ctime_t time_delta, ssize_t
 
 // ----------------------------------------------------------------------------
 int
-py_thread_allocate_stack(void) {
+py_thread_allocate(void) {
   if (isvalid(_stack))
     SUCCESS;
 
@@ -534,16 +713,40 @@ py_thread_allocate_stack(void) {
     FAIL;
   #endif
 
+  #ifdef NATIVE
+  size_t max = pid_max();
+  _tids = (void **) calloc(max, sizeof(void *));
+  if (!isvalid(_tids))
+    FAIL;
+
+  _tids_idle = (unsigned char *) calloc(max >> 8, sizeof(unsigned char*));
+  if (!isvalid(_tids_idle))
+    FAIL;
+  #endif
+
   SUCCESS;
 }
 
 
 // ----------------------------------------------------------------------------
 void
-py_thread_free_stack(void) {
+py_thread_free(void) {
   #if defined PL_WIN
   sfree(_pi_buffer);
   #endif
 
   sfree(_stack);
+
+  #ifdef NATIVE
+  pid_t max_pid = pid_max();
+  for (pid_t tid = 0; tid < max_pid; tid++) {
+    if (isvalid(_tids[tid])) {
+      _UPT_destroy(_tids[tid]);
+      ptrace(PTRACE_DETACH, tid, 0, 0);
+      log_d("ptrace: thread %ld detached", tid);
+    }
+  }
+  sfree(_tids);
+  sfree(_tids_idle);
+  #endif
 }

.gitignore
@@ -36,6 +36,7 @@ stamp-h1
 *.gcda
 
 src/austin
+src/austinp
 src/austin.exe
 
 

src/Makefile.am
@@ -32,6 +32,7 @@ austin_SOURCES = \
   logging.c      \
   version.c      \
   stats.c        \
+  platform.c     \
   py_proc_list.c \
   py_proc.c      \
   py_thread.c

src/argparse.c
@@ -29,8 +29,11 @@
 #include "hints.h"
 #include "platform.h"
 
-
+#ifdef NATIVE
+#define DEFAULT_SAMPLING_INTERVAL  10000  // reduces impact on tracee
+#else
 #define DEFAULT_SAMPLING_INTERVAL    100
+#endif
 #define DEFAULT_INIT_RETRY_CNT       100
 
 const char SAMPLE_FORMAT_NORMAL[]      = ";%s:%s:%d";
@@ -53,6 +56,9 @@ parsed_args_t pargs = {
   /* exposure            */ 0,
   /* pipe                */ 0,
   /* gc                  */ 0,
+  #ifdef NATIVE
+  /* kernel              */ 0,
+  #endif
 };
 
 static int exec_arg = 0;
@@ -219,9 +225,15 @@ static struct argp_option options[] = {
     "Pipe mode. Use when piping Austin output."
   },
   {
-    "gc",         'g', NULL,          0,
+    "gc",           'g', NULL,          0,
     "Sample the garbage collector state."
   },
+  #ifdef NATIVE
+  {
+    "kernel",       'k', NULL,          0,
+    "Sample the kernel call stack."
+  },
+  #endif
   #ifndef PL_LINUX
   {
     "help",         '?', NULL
@@ -330,6 +342,12 @@ parse_opt (int key, char *arg, struct argp_state *state)
   case 'g':
     pargs.gc = 1;
     break;
+  
+  #ifdef NATIVE
+  case 'k':
+    pargs.kernel = 1;
+    break;
+  #endif
 
   case ARGP_KEY_ARG:
   case ARGP_KEY_END:
@@ -662,6 +680,8 @@ cb(const char opt, const char * arg) {
 // ----------------------------------------------------------------------------
 int
 parse_args(int argc, char ** argv) {
+  pargs.output_file = stdout;
+
   #ifdef PL_LINUX
   struct argp args = {options, parse_opt, "command [ARG...]", doc};
   argp_parse(&args, argc, argv, 0, 0, 0);

src/argparse.h
@@ -27,6 +27,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 
+#include "platform.h"
 #include "stats.h"
 
 typedef struct {
@@ -44,6 +45,9 @@ typedef struct {
   ctime_t   exposure;
   int       pipe;
   int       gc;
+  #ifdef NATIVE
+  int       kernel;
+  #endif
 } parsed_args_t;
 
 

src/austin.c
@@ -75,8 +75,12 @@ do_single_process(py_proc_t * py_proc) {
 
       if (fail(py_proc__sample(py_proc)))
         break;
-
+      
+      #ifdef NATIVE
+      stopwatch_pause(0);
+      #else
       stopwatch_pause(stopwatch_duration());
+      #endif
     }
   }
   else {
@@ -88,7 +92,11 @@ do_single_process(py_proc_t * py_proc) {
       if (fail(py_proc__sample(py_proc)))
         break;
 
+      #ifdef NATIVE
+      stopwatch_pause(0);
+      #else
       stopwatch_pause(stopwatch_duration());
+      #endif
 
       if (end_time < gettime())
         interrupt++;
@@ -154,20 +162,32 @@ do_child_processes(py_proc_t * py_proc) {
 
   if (pargs.exposure == 0) {
     while (!py_proc_list__is_empty(list) && interrupt == FALSE) {
+      #ifndef NATIVE
       ctime_t start_time = gettime();
+      #endif
       py_proc_list__update(list);
       py_proc_list__sample(list);
+      #ifdef NATIVE
+      stopwatch_pause(0);
+      #else
       stopwatch_pause(gettime() - start_time);
+      #endif
     }
   }
   else {
     log_m("🕑 Sampling for %d second%s", pargs.exposure, pargs.exposure != 1 ? "s" : "");
     ctime_t end_time = gettime() + pargs.exposure * 1000000;
     while (!py_proc_list__is_empty(list) && interrupt == FALSE) {
+      #ifndef NATIVE
       ctime_t start_time = gettime();
+      #endif
       py_proc_list__update(list);
       py_proc_list__sample(list);
+      #ifdef NATIVE
+      stopwatch_pause(0);
+      #else
       stopwatch_pause(gettime() - start_time);
+      #endif
 
       if (end_time < gettime()) interrupt++;
     }
@@ -212,7 +232,7 @@ int main(int argc, char ** argv) {
     goto finally;
   }
 
-  if (fail(py_thread_allocate_stack())) {
+  if (fail(py_thread_allocate())) {
     log_ie("Cannot allocate memory for thread stack");
     goto finally;
   }
@@ -244,9 +264,7 @@ int main(int argc, char ** argv) {
     goto finally;
 
   // Redirect output to STDOUT if not output file was given.
-  if (pargs.output_file == NULL)
-    pargs.output_file = stdout;
-  else
+  if (pargs.output_file != stdout)
     log_i("Output file: %s", pargs.output_filename);
 
   log_i("Sampling interval: %lu μs", pargs.t_sampling_interval);
@@ -298,7 +316,7 @@ int main(int argc, char ** argv) {
   stats_log_metrics();NL;
 
 finally:
-  py_thread_free_stack();
+  py_thread_free();
   py_proc__destroy(py_proc);
 
   log_d("Last error: %d :: %s", austin_errno, get_last_error());
@@ -345,13 +363,14 @@ finally:
     retval = SIGTERM;
 
   log_footer();
-  logger_close();
 
 release:
   if (pargs.output_file != NULL && pargs.output_file != stdout) {
     fclose(pargs.output_file);
     log_d("Output file closed.");
   }
+
+  logger_close();
 
   return retval;
 } /* main */

src/linux/py_proc.h
@@ -35,6 +35,9 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+#ifdef NATIVE
+#include "../argparse.h"
+#endif
 #include "../dict.h"
 #include "../hints.h"
 #include "../py_proc.h"
@@ -377,13 +380,11 @@ _py_proc__parse_maps_file(py_proc_t * self) {
   while (getline(&line, &len, fp) != -1) {
     ssize_t lower, upper;
     char    pathname[1024];
-    char    m[sizeof(void *)]; // We don't care about these values.
 
-    int field_count = sscanf(line, "%lx-%lx %4c %lx %x:%x %x %s\n",
-      &lower, &upper,                                             // Map bounds
-      (char *) m, (ssize_t *) m, (int *) m, (int *) m, (int *) m, // Ignored
-      pathname                                                    // Binary path
-    ) - 7; // We expect between 7 and 8 matches.
+    int field_count = sscanf(line, "%lx-%lx %*s %*x %*x:%*x %*x %s\n",
+      &lower, &upper, // Map bounds
+      pathname        // Binary path
+    ) - 3; // We expect between 3 and 4 matches.
     if (field_count >= 0) {
       if (field_count == 0 || strstr(pathname, "[v") == NULL) {
         // Skip meaningless addresses like [vsyscall] which would give
@@ -488,6 +489,51 @@ _py_proc__get_resident_memory(py_proc_t * self) {
 } /* _py_proc__get_resident_memory */
 
 
+#ifdef NATIVE
+// ----------------------------------------------------------------------------
+static int
+_py_proc__dump_maps(py_proc_t * self) {
+  char      file_name[32];
+  FILE    * fp        = NULL;
+  char    * line      = NULL;
+  size_t    len       = 0;
+
+  sprintf(file_name, "/proc/%d/maps", self->pid);
+  fp = fopen(file_name, "r");
+  if (fp == NULL) {
+    switch (errno) {
+    case EACCES:  // Needs elevated privileges
+      set_error(EPROCPERM);
+      break;
+    case ENOENT:  // Invalid pid
+      set_error(EPROCNPID);
+      break;
+    default:
+      set_error(EPROCVM);
+    }
+    FAIL;
+  }
+
+  while (getline(&line, &len, fp) != -1) {
+    ssize_t lower, upper;
+    char    pathname[1024];
+
+    if (sscanf(line, "%lx-%lx %*s %*x %*x:%*x %*x %s\n",
+      &lower, &upper, // Map bounds
+      pathname        // Binary path
+    ) == 3 && pathname[0] != '[') {
+      fprintf(pargs.output_file, "# map: %lx-%lx %s\n", lower, upper, pathname);
+    }
+  }
+
+  sfree(line);
+  fclose(fp);
+
+  SUCCESS;
+} /* _py_proc__dump_maps */
+#endif
+
+
 // ----------------------------------------------------------------------------
 static int
 _py_proc__init(py_proc_t * self) {
@@ -502,6 +548,10 @@ _py_proc__init(py_proc_t * self) {
   sprintf(self->extra->statm_file, "/proc/%d/statm", self->pid);
 
   self->last_resident_memory = _py_proc__get_resident_memory(self);
+
+  #ifdef NATIVE
+  _py_proc__dump_maps(self);
+  #endif
 
   SUCCESS;
 } /* _py_proc__init */

src/platform.c
@@ -1,0 +1,30 @@
+#include <stdio.h>
+
+#include "hints.h"
+#include "platform.h"
+
+
+// ----------------------------------------------------------------------------
+size_t
+pid_max() {
+  #if defined PL_LINUX                                               /* LINUX */
+  FILE * pid_max_file = fopen("/proc/sys/kernel/pid_max", "rb");
+  if (!isvalid(pid_max_file))
+    return 0;
+
+  size_t max_pid;
+  int has_pid_max = (fscanf(pid_max_file, "%ld", &max_pid) == 1);
+  fclose(pid_max_file);
+  if (!has_pid_max)
+    return 0;
+
+  return max_pid;
+
+  #elif defined PL_MACOS                                             /* MACOS */
+  return PID_MAX;
+
+  #elif defined PL_WIN                                                 /* WIN */
+  return (1 << 22);  // 4M.  WARNING: This could potentially be violated!
+
+  #endif
+}

commit ab775ec12ad6a49b40660166c340890aa6db852c

fix(frame): manage frame stack correctly
This fixes a regression introduced by #85.

src/py_thread.c
@@ -319,7 +319,7 @@ _py_frame_fill_from_raddr(raddr_t * raddr) {
     &(self->code), &py_code_raddr, V_FIELD(int, frame, py_frame, o_lasti)
   )) {
     log_ie("Cannot get PyCodeObject for frame");
-    SUCCESS;
+    goto push;
   }
 
   self->raddr.pid  = raddr->pid;
@@ -330,6 +330,7 @@ _py_frame_fill_from_raddr(raddr_t * raddr) {
 
   self->invalid = 0;
 
+push:
   _stackp++;
 
   SUCCESS;