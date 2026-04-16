46dd049ee2f30e261453c97b8a216b21266b12a5
Gabriele N. Tornetta <P403n1x87@users.noreply.github.com>
2026-04-16 14:06:14 +0100

    chore: remove hard-coded path in run script (#376)

--- test/run.sh
+++ test/run.sh
@@ -9,6 +9,5 @@ fi
 
 source /tmp/austin-test/bin/activate
 ulimit -c unlimited
-export LD_LIBRARY_PATH="/Users/gabriele/Projects/libunwind/src/.libs/"
 AUSTIN_TESTS_PYTHON_VERSIONS=$PY pytest "$@"
 deactivate
