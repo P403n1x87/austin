f55e2196d4e8cbbe8805ce197a7bc0f9bfbd1545
Gabriele N. Tornetta <gabriele.tornetta@datadoghq.com>
2026-04-30 19:13:45 +0100

    chore: drop Python 3.9 support
    Python 3.9 has reached EOL in October 2025 so we drop support in
    the next Austin release. We also bump our test harnesses to 3.13
    for gain some leeway.

--- scripts/build-wheel.py
+++ scripts/build-wheel.py
@@ -16,7 +16,6 @@ METADATA = {
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
